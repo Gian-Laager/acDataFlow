4228fe540755d32bcb08931153516fb764855ad4
Gian <gianlaager114@gmail.com>
2020-06-14 20:03:40 +0200

    installed sycl. Can't be build yet, something with openMp went wrong

---
+++ cmake-build-release/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/gianlaager/Documents/code/C++/ClionProjects/acDataFlow
+# Build directory: /Users/gianlaager/Documents/code/C++/ClionProjects/acDataFlow/cmake-build-release
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
