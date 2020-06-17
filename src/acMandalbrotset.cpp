96ee215496b5d3b2a12a0d501b4c246b581be47a
Gian <gianlaager114@gmail.com>
2020-06-17 23:16:46 +0200

    new Shader class (copied from acGameEngine)

--- src/acMandalbrotset.cpp
+++ src/acMandalbrotset.cpp
@@ -76,14 +76,11 @@ void helloSycl()
 
 int main()
 {
-//    helloSycl();
     GLFWwindow* window;
 
-    /* Initialize the library */
     if (!glfwInit())
         return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
     {
@@ -91,13 +88,10 @@ int main()
         return -1;
     }
 
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
         glBegin(GL_POLYGON);
@@ -107,10 +101,8 @@ int main()
         glVertex3f(0.0, 0.5, 0.0);
         glEnd();
 
-        /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
-        /* Poll for and process events */
         glfwPollEvents();
     }
 
